a6bc7b089edacecc1ab4baf903612cb27cff87d4
Carlosrzz <josue.exejj@gmail.com>
2025-07-23 15:45:08 -0600

    Update Main.java

--- Rodriguez_20250721/src/rodriguez_20250721/Main.java
+++ Rodriguez_20250721/src/rodriguez_20250721/Main.java
@@ -17,9 +17,16 @@ public class Main {
         // TODO code application logic here
         System.out.println("50 grados centigrados a Farentheit:" + Celsius_a_fahrenheit() + "F");
          System.out.println("50 grados centigrados a Kelvin:" + Celsius_a_Kelvin() + "K");
-         System.out.println("50 grados centigrados a Rankine:" + Celsius_a_Rankine() + "R");
-         System.out.println("50 grados Farentheit a centigrados:" + fahrenheit_a_Celsius () + "C");
-         System.out.println("50 grados farenheit a Kelvin:" + fahrenheit_a_Kelvin () + "K");
+         System.out.println("50 grados centigrados a Rankine:" + Celsius_a_Rankine() + "RA");
+         System.out.println("30 grados Farentheit a centigrados:" + fahrenheit_a_Celsius () + "C");
+         System.out.println("30 grados farenheit a Kelvin:" + fahrenheit_a_Kelvin () + "K");
+        System.out.println("30 grados farenheit a Rankine:" + fahrenheit_a_Rankine () + "RA");
+            System.out.println("30 grados Kelvin a centigrados:" + Kelvin_a_Centigrados () + "C");
+           System.out.println("30 grados Kelvin a farenheit:" +  Kelvin_a_fahrenheit () + "F");
+          System.out.println("30 grados Kelvin a Rankine:" +  Kelvin_a_Rankine () + "RA");
+        System.out.println("30 grados Rankine a Centigrados:" +  Rankine_a_Centigrados () + "C");
+         System.out.println("30 grados Rankine a farenheit:" +  Rankine_a_fahrenheit () + "F");
+        System.out.println("30 grados Rankine a Kelvin:" +  Rankine_a_Kelvin () + "K");
     }
     
     
@@ -48,4 +55,52 @@ public class Main {
         double grados_centigrados = 30.0;
         return ((grados_centigrados -32)* 5)/9 +273.15;
     }   
+
+
+        public static double fahrenheit_a_Rankine.(){
+        double grados_centigrados = 30.0;
+        return ((grados_centigrados -32)* 9)/5) + 459.67;
+    }   
+
+
+         public static double Kelvin_a_Centigrados.(){
+        double grados_centigrados = 30.0;
+        return (grados_centigrados) - 273.15;
+    }   
+
+
+             public static double Kelvin_a_fahrenheit.(){
+        double grados_centigrados = 30.0;
+        return ((grados_centigrados) - 273.15) * 9/5 + 32;
+    }   
+
+
+           public static double Kelvin_a_Rankine.(){
+        double grados_centigrados = 30.0;
+        return ((grados_centigrados * 1.8);
+    }   
+
+
+      public static double Rankine_a_Centigrados.(){
+        double grados_centigrados = 30.0;
+        return (grados_centigrados - 491.67) * 5/9;
+    }   
+
+
+
+     public static double Rankine_a_fahrenheit.(){
+        double grados_centigrados = 30.0;
+        return (grados_centigrados - 459.67);
+    }   
+
+
+
+    
+     public static double Rankine_a_Kelvin.(){
+        double grados_centigrados = 30.0;
+        return (grados_centigrados) * 5/9 ;
+    }   
+
+    
+    
 }
